3c686503e58762a2e25a502664834d35822f532e
divyanshuSukuna <divyanshugd47@gmail.com>
2023-10-28 18:29:49 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+
+export {Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality, CustomerReviews, Footer}
